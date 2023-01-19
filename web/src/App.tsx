8199ddedd2b8ef0d59f30ef8b314d4509c9705be
braiancalot <calot665@hotmail.com>
2023-01-19 07:36:18 -0300

    feat(web): setup

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="bg-black">
+      <span className="text-purple-500">Opa veio</span>
+    </div>
+  )
+}
+
+export default App
