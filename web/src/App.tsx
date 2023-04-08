640ef4344ef495074e9718db5f863a86904b3d04
braiancalot <calot665@hotmail.com>
2023-04-08 15:02:05 -0300

    feat(mobile): setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
+import { Habit } from "./components/Habit";
 import "./styles/global.css";
 
 function App() {
   return (
-    <div className="bg-black">
-      <span className="text-purple-500">Opa veio</span>
-    </div>
+    <>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </>
   )
 }
 
